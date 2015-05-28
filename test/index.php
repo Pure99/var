049228a24296d28a3a06697ae79965fc880ac587
Pure99 <pure.denis@gmail.com>
2015-05-28 16:51:39 +0600

    2-е задание

--- test/index.php
+++ test/index.php
@@ -1,10 +1,27 @@
 <?php
-$connection = new mysqli('localhost', 'root', '', 'test'); // устанавливаем соединение с БД
+/**
+* Создаем подключение к базе данных
+*/
+$connection = new mysqli('localhost', 'root', '', 'test'); 
 if (mysqli_connect_errno()) {
     printf("Ошибка подключения к базе данных: %s\n", mysqli_connect_error());
     exit();
 }
+/**
+* Указываем кодировку подключения
+*/
+$connection->set_charset("utf8");
+/**
+* Задача №1
+*
+* Класс init
+*/
 	final class init {
+		/**
+		* Метод create()
+		* Доступен только для методов класса
+		* Создает таблицу 'test'
+		*/
 	private function create() {
 	global $connection;
 	$connection->query( "CREATE TABLE `test` (
@@ -13,21 +30,34 @@ if (mysqli_connect_errno()) {
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   `result` text COLLATE 'utf8_unicode_ci' NOT NULL
-) COLLATE 'utf8_unicode_ci';"); //создать таблицу
+) COLLATE 'utf8_unicode_ci';"); 
 	}
+		/**
+		* Метод fill()
+		* Доступен только для методов класса
+		* Вставляет в тыблицу 10 строк с случайными значениями
+		*/
 	private function fill() {
-	global $connection;
+		global $connection;
         $result = array ('normal', 'illegal', 'failed', 'success');
 	for ($k=1; $k<11; $k++){
         $x=mt_rand(0, 3);
-	$connection->query("INSERT INTO `test` (`script_name`, `start_time`, `end_time`, `result`) VALUES (ROUND((RAND() * (1000 - 0)) + 0), ROUND((RAND() * (1000 - 0)) + 0), ROUND((RAND() * (1000 - 0)) + 0), '');");  // записать 
-        $connection->query("UPDATE `test` SET `result`='$result[$x]' where `id`='$k';");
+	$connection->query("INSERT INTO `test` (`script_name`, `start_time`, `end_time`, `result`) VALUES (ROUND((RAND() * (1000 - 0)) + 0), ROUND((RAND() * (1000 - 0)) + 0), ROUND((RAND() * (1000 - 0)) + 0), '$result[$x]');"); 
 		}
 	}
+		/**
+		* Конструктор 
+		* Содержит методы create() и fill()
+		*/
 		function __construct() {
 			$this->create();
 			$this->fill();
 		}
+		/**
+		* Метод get()
+		* Доступен извне класса
+		* Выбирает из таблицы test, данные по критерию: result среди значений 'normal' и 'success'
+		*/
 	public function get(){
 	global $connection;
 	$result = $connection->query("select * from test where result='normal' or result='success';");
@@ -42,11 +72,17 @@ echo "</table>";
         $test->get();
 ?>
 <?php
+/**
+* Задача №3
+*
+* Открываем каталог datafiles
+*/
 if ($handle = opendir('./datafiles/')) {
     echo "Дескриптор каталога: $handle<br>";
     echo "Файлы:<br>";
-
-    /* Именно этот способ чтения элементов каталога является правильным. */
+/**
+* Читаем каталог datafiles и выводим имена файлов, состоящие из цифр и букв латинского алфавита, с расширением ixt
+*/
     while (false !== ($file = readdir($handle))) { 
 	if ($file != "." && $file != ".." &&  preg_match("/\.(:?ixt)$/i", $file, $matches) && ! preg_match("/[^a-z0-9\.]+/", $file, $matches)) {
         echo "$file<br>";
@@ -56,19 +92,31 @@ closedir($handle);
 }
 ?>
 <?php
+/**
+* Задача №4
+*/
+
+/**
+* Функция преобразования строки с датой на русском в дату формата "Y-m-d"
+*/
 function newFormatDate($date) {
     $date = str_replace(array('янв', 'Фев', 'мар', 'апр', 'мая', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'),
                         array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
                         $date);
 return date("Y-m-d", strtotime($date));
 }
-$connection->set_charset("utf8");
+/**
+* Создаем табдицу 'bills_ru_events'
+*/
 $connection->query( "CREATE TABLE `bills_ru_events` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `date` datetime  NOT NULL,
   `title` char(230) COLLATE 'utf8_unicode_ci' NOT NULL,
   `url` char(240) COLLATE 'utf8_unicode_ci' NOT NULL UNIQUE)
- ENGINE=MyISAM DEFAULT COLLATE 'utf8_unicode_ci';"); //создать таблицу
+ ENGINE=MyISAM DEFAULT COLLATE 'utf8_unicode_ci';"); 
+/**
+* Создаем новый объект и загружаем содержимое сайта
+*/
 $doc = new DOMDocument();
 @$doc->loadHTMLFile('http://www.bills.ru/');
 $tags_a = $doc->getElementById('bizon_api_news_list')->getElementsByTagName('a');
@@ -80,6 +128,8 @@ for ($c = 0; $c<$tags_a->length; $c++  ) {
 }
 echo "</table aligne>";
 ?>
+
+<!-- Задача №5      -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <table>
 <tr><td id='11'><button id='1' style="text-align: center">1</button></td></tr>
@@ -87,10 +137,11 @@ echo "</table aligne>";
 <tr><td id='33'><button id='3' style="text-align: center">3</button></td></tr>
 </table>
 <script>
-    $('#1, #2, #3').click(function(){ var b1 = $('#1').detach(); var b2 = $('#2').detach(); var b3 = $('#3').detach();
-  
-b1.appendTo('#11');
-b2.appendTo('#22');
-b3.appendTo('#33');
+    $('#1, #2, #3').click(function(){ 
+	var m = [$('#11').children().detach(), $('#22').children().detach(), $('#33').children().detach()]; // создаем массив с кнопками по порядку
+	m = m.slice(1).concat(m.slice(0,1));  //сдвигаем массив
+	m[0].appendTo('#11'); // всатвляем кнопку
+	m[1].appendTo('#22');
+	m[2].appendTo('#33');
     });
 </script>
