5af10bd8c590551340819d589f1bff07c27a567b
Pure99 <pure.denis@gmail.com>
2015-05-29 16:53:03 +0600

    задача 2

--- test/index.php
+++ test/index.php
@@ -73,6 +73,19 @@ echo "</table>";
 ?>
 <?php
 /**
+* Задача №2
+*
+*/
+$connection->query( "CREATE TABLE `info` ( `id` int(11) NOT NULL auto_increment, `name` varchar(255) default NULL, `desc` text default NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=cp1251;"); 
+$connection->query( "CREATE TABLE `data` (`id` int(11) NOT NULL auto_increment, `date` date default NULL, `value` INT(11) default NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=cp1251;"); 
+$connection->query( "CREATE TABLE `link` (`data_id` int(11) NOT NULL,`info_id` int(11) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=cp1251;");
+$connection->query( "select * from data,link,info where link.info_id = info.id and link.data_id = data.id;");
+$connection->query( "INSERT INTO `test`.`info` (`data_id`,) SELECT `data_id` FROM `test`.`link` ;");
+$connection->query( "INSERT INTO `test`.`data` (`info_id`,) SELECT `info_id` FROM `test`.`link` ;");
+
+?>
+<?php
+/**
 * Задача №3
 *
 * Открываем каталог datafiles
